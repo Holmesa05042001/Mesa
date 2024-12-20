4af7d1af0947e74ff822fb8b6d0d470c9c9dad68
Holmesa05042001 <mesahol178@gmail.com>
2024-12-20 14:23:00 +0700

    Add files via upload

---
+++ mesa.js
@@ -0,0 +1,8 @@
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    alert(`Thank you, ${name}! We have received your message: "${message}". We will get back to you at ${email} soon.`);
+});
